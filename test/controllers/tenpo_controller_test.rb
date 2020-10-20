d9f8f51f51c19d503e559c0e953576b9a34e4314
s-itonaga <sitonaga914@gmail.com>
2020-10-20 18:55:36 +0900

    Rubocop修正

--- test/controllers/tenpo_controller_test.rb
+++ test/controllers/tenpo_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class TenpoControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get tenpo_index_url
     assert_response :success
   end
-
 end
